86e1f8532d0ae4845a7ae4c6e5faed75a3f7c70e
Hans Eric Lippke <helippke@betteru.ca>
2021-12-02 20:29:07 +0000

    Added load tweets function to grab the tweets form the server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -41,35 +41,18 @@ const renderTweets = function (tweets) {
     }
 };
 
-// Fake data taken from initial-tweets.json
-const data = [
-    {
-        user: {
-            name: "Newton",
-            avatars: "https://i.imgur.com/73hZDYK.png",
-            handle: "@SirIsaac",
-        },
-        content: {
-            text: "If I have seen further it is by standing on the shoulders of giants",
-        },
-        created_at: 1461116232227,
-    },
-    {
-        user: {
-            name: "Descartes",
-            avatars: "https://i.imgur.com/nlhLi3I.png",
-            handle: "@rd",
-        },
-        content: {
-            text: "Je pense , donc je suis",
-        },
-        created_at: 1461113959088,
-    },
-];
+const loadTweets = function () {
+    // get the tweets from the server
+    $.get("/tweets", function (data) {
+        console.log(data);
+        // render the tweets
+        renderTweets(data);
+    });
+};
 
 //wait for the document to be ready, then render the tweets
 $(document).ready(() => {
-    renderTweets(data);
+    loadTweets();
 
     $(function () {
         const $form = $(".new-tweet form");
